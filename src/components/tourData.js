97c3130c93be860fb1193e7da714f57a5123652b
Hongyanlee0614 <hongyanlee0614@gmail.com>
2021-01-15 18:25:12 +0800

    Initial Commit

---
+++ src/components/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "The Brooklyn Bridge is a hybrid cable-stayed/suspension bridge in New York City, spanning the East River between the boroughs of Manhattan and Brooklyn"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "At the Paris Exhibition of 1900, it was still doubtful whether lighting by incandescent gas mantles or open arc lamps was the better"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Buckingham Palace is the London residence and administrative headquarters of the monarch of the United Kingdom. Located in the City of Westminster, the palace is often at the centre of state occasions and royal hospitality"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "One of the world's largest seafood markets with a guide as you learn how fish is caught, distributed, and sold"
+  }
+];
