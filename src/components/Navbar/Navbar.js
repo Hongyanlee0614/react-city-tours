187d4744a6f69dd10d6b6cddb5cbe7f78a1bfc02
Hongyanlee0614 <hongyanlee0614@gmail.com>
2021-01-15 18:39:57 +0800

    remove about

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,6 @@ export default function Navbar() {
             <img src={logo} alt='city tours logo'></img>
             <ul className="nav-links">
                 <li><a href="/" className="nav-link">Home</a></li>
-                <li><a href="/" className="nav-link">About</a></li>
                 <li><a href="/" className="nav-link active">Tours</a></li>
             </ul>
         </nav>
